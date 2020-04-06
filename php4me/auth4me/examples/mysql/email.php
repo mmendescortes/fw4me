e760c6e71ef8835fa19df23c5d2c7f851e5aa5eb
Mateus M. Côrtes <mmendescortes@horse.local>
2020-04-06 11:02:41 -0300

    Forgotten e-mail file.

---
+++ php4me/auth4me/examples/mysql/email.php
@@ -0,0 +1,9 @@
+<?php
+	require_once("settings.php");
+	require_once("../../providers/sql4me.php");
+	require_once("../../auth4me.php");
+	$database = new database;
+	$provider = new sql4me();
+	$auth = new auth4me($provider);
+	echo $auth->email("the_user_id","the_user_email")[1];
+?>
