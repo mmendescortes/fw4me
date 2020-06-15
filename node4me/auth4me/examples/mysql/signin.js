2db733ead7549a780c8cc7ee32626f6a1de99759
Mateus M. Côrtes <mmendescortes@gmail.com>
2020-06-14 22:36:58 -0300

    Fix path on require

--- node4me/auth4me/examples/mysql/signin.js
+++ node4me/auth4me/examples/mysql/signin.js
@@ -1,5 +1,5 @@
-const sql4me = require("./sql4me");
-const auth4me = require("./auth4me");
+const sql4me = require("../../providers/sql4me");
+const auth4me = require("../../auth4me");
 let provider = new sql4me();
 let auth = new auth4me(provider);
 auth.signin("the_username_or_email","the_user_password").then((r)=>{console.log(r)});
