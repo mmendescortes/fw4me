756456969639ff18a9ec90d796befa05e2f73e3d
Mateus M. Côrtes <mmendescortes@gmail.com>
2020-06-14 22:36:12 -0300

    Create signin.js

---
+++ node4me/auth4me/examples/mysql/signin.js
@@ -0,0 +1,5 @@
+const sql4me = require("./sql4me");
+const auth4me = require("./auth4me");
+let provider = new sql4me();
+let auth = new auth4me(provider);
+auth.signin("the_username_or_email","the_user_password").then((r)=>{console.log(r)});
