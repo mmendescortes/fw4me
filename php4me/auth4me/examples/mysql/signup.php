ea81bbe97bb404028b046dd39902ecaac1984007
Mateus M. Côrtes <mmendescortes@gmail.com>
2020-09-24 22:51:29 -0300

    Update signup.php

--- php4me/auth4me/examples/mysql/signup.php
+++ php4me/auth4me/examples/mysql/signup.php
@@ -1,9 +1,9 @@
 <?php
-	require_once("settings.php");
-	require_once("../../providers/sql4me.php");
-	require_once("../../auth4me.php");
-	$database = new database;
-	$provider = new sql4me();
-	$auth = new auth4me($provider);
+        require_once("../../../sql4me/sql4me.php");
+        require_once("../../../sql4auth/sql4auth.php");
+        require_once("../../auth4me.php");
+        $engine = new sql4me();
+        $provider = new sql4auth($engine);
+        $auth = new auth4me($provider);
 	echo $auth->signup("the_username","the_user_password","the_user_email_is_optional_you_can_delete_this_argument_if_you_want")[1];
 ?>
